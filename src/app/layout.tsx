c95bb0f31714882e356ca6249e4609bccd376d97
Brooke Dixon <bdixon@gmail.com>
2024-10-02 07:03:34 -0400

    Integrate Sonner for toast notifications; remove legacy form components; implement Toaster for enhanced user feedback in UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { TRPCReactProvider } from "~/trpc/react";
-
+import { Toaster } from "~/components/ui/toaster"
 import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 
@@ -13,6 +13,7 @@ export default function RootLayout({
       <html lang="en" className={`${GeistSans.variable}`}>
         <body>
           <TRPCReactProvider>{children}</TRPCReactProvider>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
