fe237c03dd50cfa82b8dd17f7a3686050e327df0
Brooke Dixon <bdixon@gmail.com>
2024-10-03 04:02:36 -0400

    Add HeaderComponent and ListComponent to common exports; enhance modularity and component accessibility in the project

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,8 @@ import AlertComponent from './alert'
 import BreadcrumbsComponent from './breadcrumbs'
 import FooterComponent from './footer'
 import FormComponent from './form'
+import HeaderComponent from './header'
+import ListComponent from './list'
 import NavbarComponent from './navbar'
 
 export {
@@ -11,5 +13,7 @@ export {
     BreadcrumbsComponent,
     FooterComponent,
     FormComponent,
+    HeaderComponent,
+    ListComponent,
     NavbarComponent
 } 
