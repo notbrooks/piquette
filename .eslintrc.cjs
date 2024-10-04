1972456cc6734ced804bb00ab9284786f2050219
Brooke Dixon <bdixon@gmail.com>
2024-10-04 06:54:33 -0400

    Enhance ESLint configuration: extend with Next.js support and update rules for consistent TypeScript linting practices

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,15 @@ const config = {
     "drizzle"
   ],
   "extends": [
+    "next",
     "next/core-web-vitals",
+    "next/typescript",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
