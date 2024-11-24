e00971543670160694771c7af867297fc8b1b7a9
Brooke Dixon <bdixon@gmail.com>
2024-11-24 04:55:29 -0500

    Removed builder config due to build errors.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+// import BuilderDevTools from "@builder.io/dev-tools/next";
+
+// /**
+//  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+//  * for Docker builds.
+//  */
+// import "./src/env.js";
+
+// /** @type {import("next").NextConfig} */
+// const config = BuilderDevTools()({
+//   images: {
+//     unoptimized: true,
+//     domains: ["img.clerk.com"],
+//   },
+// });
+
+// export default config;
 
 
 /**
