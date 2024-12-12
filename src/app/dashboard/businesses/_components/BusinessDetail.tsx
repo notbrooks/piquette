1fea530daff6a8245d7ad6934627c2520c69d33e
Brooke Dixon <bdixon@gmail.com>
2024-12-12 12:14:45 -0500

    Refactor BusinessDetail component to enhance type safety and streamline breadcrumb implementation for better clarity

--- src/app/dashboard/businesses/_components/BusinessDetail.tsx
+++ src/app/dashboard/businesses/_components/BusinessDetail.tsx
@@ -1,73 +1,35 @@
-import { businessConfig } from '../business.config'
-
-import FormComponent from "~/components/common/Form/form";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "~/components/ui/breadcrumb";
-import { Button } from "~/components/ui/button";
-import type { Profile, BusinessBase } from "~/types";
+import type { BusinessBase } from "~/types";
 
 import { Slash } from "lucide-react";
-
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-
-  } from "~/components/ui/sheet"
 
 interface BusinessDetailProps {
     business: BusinessBase;
 }
 
-export default function BusinessDetail({  business }: BusinessDetailProps) {
-    
-
-    const handleFormSubmit = (data: Record<string, unknown>) => {
-        console.log("Form submitted:", data);
-    };
-
+export default function BusinessDetail({ business }: BusinessDetailProps) {
     return (
         <div>
             <div className="pb-5 sm:flex sm:items-center sm:justify-between">
-                <div>
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator>
-                                <Slash />
-                            </BreadcrumbSeparator>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink onClick={() => window.history.back()}>Businesses</BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator>
-                                <Slash />
-                            </BreadcrumbSeparator>
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>{business.name}</BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                </div>
-                            
-                <div className="mt-3 sm:ml-4 sm:mt-0">
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button variant="outline">Edit</Button>
-                    </SheetTrigger>
-                    <SheetContent>
-                        <SheetHeader>
-                        <SheetTitle>Edit &aspo;{business.name}&apos;</SheetTitle>
-                        <SheetDescription>
-                            <FormComponent formConfig={businessConfig.form} onSubmit={handleFormSubmit} isFormLoading={false} />
-                        </SheetDescription>
-                        </SheetHeader>
-                    </SheetContent>
-                </Sheet>
-                </div>
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>
+                            <Slash />
+                        </BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink onClick={() => window.history.back()}>Businesses</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>
+                            <Slash />
+                        </BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>{business.name}</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
             </div>
             <div>
                 <p>{business.description}</p>
--- src/app/dashboard/organizations/_components/OrganizationDetail.tsx
+++ src/app/dashboard/organizations/_components/OrganizationDetail.tsx
@@ -1,61 +1,35 @@
-import { useParams } from 'next/navigation';
-import { api } from "~/trpc/react";
-import { organizationConfig } from '../organization.config'
-
-import FormComponent from "~/components/common/Form/form";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "~/components/ui/breadcrumb";
-import { Button } from "~/components/ui/button";
-import type { Profile, OrganizationBase } from "~/types";
+import type { OrganizationBase } from "~/types";
 
 import { Slash } from "lucide-react";
 
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-    SheetFooter,
-    SheetClose
-  } from "~/components/ui/sheet"
-
 interface OrganizationDetailProps {
-    profile: Profile; // Update profile to the correct type if needed
     organization: OrganizationBase;
 }
 
-export default function OrganizationDetail({ profile, organization }: OrganizationDetailProps) {
-    
-
-    const handleFormSubmit = (data: Record<string, unknown>) => {
-        console.log("Form submitted:", data);
-    };
-
+export default function OrganizationDetail({ organization }: OrganizationDetailProps) {
     return (
         <div>
             <div className="pb-5 sm:flex sm:items-center sm:justify-between">
-                <div>
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator>
-                                <Slash />
-                            </BreadcrumbSeparator>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink onClick={() => window.history.back()}>Organizations</BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator>
-                                <Slash />
-                            </BreadcrumbSeparator>
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>{organization.name}</BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                </div>
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>
+                            <Slash />
+                        </BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink onClick={() => window.history.back()}>Organizations</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator>
+                            <Slash />
+                        </BreadcrumbSeparator>
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>{organization.name}</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
             </div>
             <div>
                 <p>{organization.description}</p>
